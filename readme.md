218aa90d42fc890561e19d8306a3aabceb24055b
Josiah S Greenwell <jsgreenwell83@gmail.com>
2020-10-29 11:48:06 -0400

    added readme and reverted to version one for lecture.

---
+++ readme.md
@@ -0,0 +1,17 @@
+### Lists, Files, and Loops
+Very basic intro into using ArrayLists to read and
+load data from .csv files.
+
+## Three Versions:
+
+#### First
+No file just simple Array vs. List operations (totaling).
+
+#### Second
+Adding in List of Ints to read in file (no csv just text).
+
+#### Third
+Final version uses a List of Lists (int) which reads and
+splits data as you would see in a CSV file.
+
+EXAMPLE CODE - NOT FOR PRODUCTION.
--- src/edu/cop2006/arraysandloops/Main.java
+++ src/edu/cop2006/arraysandloops/Main.java
@@ -1,49 +1,22 @@
 package edu.cop2006.arraysandloops;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 class Main
 {
 
-  public static void main(String[] args) throws FileNotFoundException {
-    List<List<Integer>> results = new ArrayList<>();
+  public static void main(String[] args) {
+    // int[] intArray = [5, 25, 10]
 
-    FileReader reader = new FileReader("src/edu/cop2006/arraysandloops/results.txt");
+    ArrayList<Integer> intList = new ArrayList<>();
 
-    try(BufferedReader resultReader = new BufferedReader(reader)) {
-      String line; // remember we're reading from a "text" file so we have to convert
-
-      while ((line = resultReader.readLine()) != null) {
-        List<Integer> numbers = new ArrayList<>(); // remember strings cause textfile
-        for (String number : line.split(",")) {
-          numbers.add(Integer.parseInt(number));
-          // you can also use (int) but this less error prone
-        }
-        results.add(numbers);
-      }
-    } catch (IOException fileEx) {
-      System.out.println("Could not load file\n" + fileEx);
-    }
+    intList.add(5);
+    intList.add(25);
+    intList.add(10);
 
     int total = 0;
-    for (List<Integer> nums : results) {
-      /* First version prints out just the totals like our first version 
-      for(int num : nums) {
-        System.out.println("Number is : " + num);
-        total += num;
-      } */
-
-      int subtotal = 0;
-      for(int i=0; i<nums.size(); i++) {
-        System.out.printf("The %d number is %d. ",
-            i+1, nums.get(i));
-        subtotal += nums.get(i);
-      }
-      System.out.printf("The total for this group was %d%n", subtotal);
+    for(int num : intList) {
+      System.out.println("Number is : " + num);
+      total += num;
     }
 
     System.out.println("total is: " + total);
