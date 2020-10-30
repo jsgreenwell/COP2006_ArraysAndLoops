0597dce8578e99e9f48a1d4bc32728a3d39fec55
Josiah S Greenwell <jsgreenwell83@gmail.com>
2020-10-29 21:25:57 -0400

    Simple (not production quality) example of using Lists of Maps.

--- src/edu/cop2006/arraysandloops/Main.java
+++ src/edu/cop2006/arraysandloops/Main.java
@@ -4,52 +4,72 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Main
 {
 
   public static void main(String[] args) throws FileNotFoundException {
-    List< List<Integer> > results = new ArrayList<>();
-    FileReader reader = new FileReader("src/results.txt");
+    List<Map<String, String>> results = new ArrayList<>();
+    FileReader reader = new FileReader("src/results.csv");
 
     try (BufferedReader resultReader = new BufferedReader(reader)) {
       String line; // holds current line
+      int headers = 0;
+      Map<String, String> sampleTests = new HashMap<>();
 
       while ((line = resultReader.readLine()) != null) {
-        List<Integer> currentLine = new ArrayList<>();
-
-        if (!line.startsWith("#")) {
+        if (headers == 0) {
           for (String field: line.split(",")) {
-            currentLine.add(Integer.parseInt(field));
+            sampleTests.put(field, "");
           }
-          results.add(currentLine);
+          headers++;
+        } else {
+          if (!line.startsWith("#")) {
+            String[] field = line.split(",");
+            for (int i=0; i<field.length;i++) {
+              switch (i) {
+                case 0:
+                  sampleTests.put("id", field[i]);
+                  break;
+                case 1:
+                  sampleTests.put("label", field[i]);
+                  break;
+                case 2:
+                  sampleTests.put("x", field[i]);
+                  break;
+                case 3:
+                  sampleTests.put("y", field[i]);
+                  break;
+                case 4:
+                  sampleTests.put("weight", field[i]);
+                  break;
+                case 5:
+                  sampleTests.put("cluster", field[i]);
+                  break;
+              }
+            }
+            results.add(sampleTests);
+          }
         }
         // if line does not start with # (comment) then:
-        // add the line to a temporay List ( Integer)
-        // Then add that list to our full results
+        // add the line to a our sample test data table
+        // Then add that map to our full results for later processing/printing
       }
     } catch (IOException fileError) {
       System.out.println("File error: " + fileError.getStackTrace());
     }
 
-    /*
-    Will print out each set (list) of numbers and the total for that set
-    Then calculate the full total of all numbers and print that
-     */
-    int allTotals = 0;
-    for(List<Integer> nums : results) {
-
-      int total = 0;
-      for (int i=0; i<nums.size(); i++) {
-        System.out.printf("The %d number is %d. ",
-            i+1, nums.get(i));
-        total += nums.get(i);
+    System.out.println("The first result set was: ");
+    for (Map<String, String> r : results) {
+      for (String k : r.keySet()) {
+        System.out.printf("The %s is %s%n",
+            k, r.get(k));
       }
-      System.out.println("Total for this row is " + total);
-      allTotals += total;
+      System.out.println("\nThe next result set was: ");
+      // Leave further prettyifying of this output to reader
     }
-
-    System.out.println("Everything totalled is: " + allTotals);
   }
 }
